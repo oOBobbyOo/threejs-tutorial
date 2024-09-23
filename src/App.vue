a7bbbe54edddc0c59819f9f85e149ac206387cdc
oOBobbyOo <944831575@qq.com>
2024-09-23 12:05:38 +0800

    feat: 03.光照对物体表面的影响

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 // import '@/tutorial/01.Threejs快速入门'
-import '@/tutorial/02.Threejs三维坐标系'
+// import '@/tutorial/02.Threejs三维坐标系'
+import '@/tutorial/03.光照对物体表面的影响'
 </script>
 
 <template>
