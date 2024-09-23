f96147500214333d2255f34376b304b0719a67df
oOBobbyOo <944831575@qq.com>
2024-09-23 15:09:15 +0800

    feat: 07.全屏和动态修改canvas宽高比

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,8 @@
 // import '@/tutorial/03.光照对物体表面的影响'
 // import '@/tutorial/04.相机控件轨道器OrbitControls'
 // import '@/tutorial/05.平行光与环境光'
-import '@/tutorial/06.动画渲染'
+// import '@/tutorial/06.动画渲染'
+import '@/tutorial/07.全屏和动态修改canvas宽高比'
 </script>
 
 <template>
