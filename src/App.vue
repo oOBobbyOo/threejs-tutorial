40167491a2179e61dbecc5b876abe3ad5211972f
oOBobbyOo <944831575@qq.com>
2024-09-23 12:19:23 +0800

    feat: 05.平行光与环境光

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,8 @@
 // import '@/tutorial/01.Threejs快速入门'
 // import '@/tutorial/02.Threejs三维坐标系'
 // import '@/tutorial/03.光照对物体表面的影响'
-import '@/tutorial/04.相机控件轨道器OrbitControls'
+// import '@/tutorial/04.相机控件轨道器OrbitControls'
+import '@/tutorial/05.平行光与环境光'
 </script>
 
 <template>
